da63b355055909821c96977c9eeee1b766c03116
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-08-25 14:35:53 +0300

    add
     basename to browserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/TaskPro">
       <App />
     </BrowserRouter>
   </React.StrictMode>
