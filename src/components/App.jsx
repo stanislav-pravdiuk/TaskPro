be29e1c0d532aba819c26c812ac7dea509905d9a
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-08-28 12:43:38 +0300

    add
    lazy
    suspense
    restrictedRoute
    privateRoute

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-// import AuthPage from "screens/authPage/AuthPage";
-import HomePage from "screens/homePage/HomePage";
-import WelcomePage from "screens/welcomePage/WelcomePage";
-import LoginForm from "./loginForm/LoginForm";
-import RegisterForm from "./registerForm/RegisterForm";
+import { lazy, Suspense, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useAuth } from "hooks/useAuth";
-import { useEffect } from "react";
 import { refreshUser } from "redux/auth/authOperations";
+import { PrivateRoute } from "../privateRoute/PrivateRoute";
+import { RestrictedRoute } from "restrictedRoute/RestrictedRoute";
+
+
+const HomePage = lazy(() => import("screens/homePage/HomePage"));
+const WelcomePage = lazy(() => import("../screens/welcomePage/WelcomePage"));
+const LoginForm = lazy(() => import("./loginForm/LoginForm"));
+const RegisterForm = lazy(() => import("./registerForm/RegisterForm"));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -17,16 +20,24 @@ export const App = () => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return isRefreshing ? (
-  <b>Refreshing user...</b>
-  ):(
-    <Routes>
-      <Route index element={<WelcomePage />} />
-      <Route path="/welcome" element={<WelcomePage />} />
-      {/* <Route path="/auth/:id" element={<AuthPage />} /> */}
-      <Route path="/auth/login" element={<LoginForm />} />
-      <Route path="/auth/register" element={<RegisterForm />} />
-      <Route path="/home" element={<HomePage />} />
-    </Routes>
+  return (
+    <Suspense fallback={<b>Загрузка...</b>}>
+      {isRefreshing ? (
+        <b>Refreshing user...</b>
+      ) : (
+        <Routes>
+          <Route index element={<RestrictedRoute component={<WelcomePage />} redirectTo="/home" />} />
+            <Route path="/auth/login"
+              element={<RestrictedRoute component={<LoginForm />} redirectTo="/home" />}
+            />
+            <Route path="/auth/register"
+              element={<RestrictedRoute component={<RegisterForm />} redirectTo="/home" />}
+            />
+            <Route path="/home"
+              element={<PrivateRoute component={<HomePage />} redirectTo="/auth/login" />}
+            />
+        </Routes>
+      )}
+    </Suspense>
   );
 };
