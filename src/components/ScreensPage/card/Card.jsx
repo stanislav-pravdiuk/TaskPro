bcf49c0a3adb07d07c51c03605212c95a300c136
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-08-29 14:26:31 +0300

    done
    card

--- src/components/ScreensPage/card/Card.jsx
+++ src/components/ScreensPage/card/Card.jsx
@@ -1,18 +1,54 @@
-import { CssBaseline } from "@mui/material";
-import { Container, Description, Title } from "./Card.styled";
+import {
+    Container,
+    Description,
+    Title,
+    BottomBar,
+    Options,
+    Elipce,
+    PriorityBox,
+    TextOptions,
+    Text,
+    IconsBox,
+    TransferRight,
+    Edit,
+    Delete
+} from "./Card.styled";
+import icon from '../../iconSvg/icon.svg';
 
 const Card = () => {
+
+    const colorPriority='#8FA1D0'
+
     return (
-        <Container>
+        <Container style={{borderLeft: `4px solid ${colorPriority}`}}>
             <Title>Lorem, ipsum dolor.</Title>
             <Description>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est optio veritatis suscipit iure quas cupiditate.</Description>
-            <CssBaseline />
-            <div>
-                <p>Priority</p>
-            </div>
-            <div>
-                <p>Deadline</p>
-            </div>
+            <BottomBar>
+                <Options>
+                    <TextOptions>Priority</TextOptions>
+                    <PriorityBox>
+                        <Elipce style={{backgroundColor:colorPriority}}></Elipce>
+                        <Text>Low</Text>
+                    </PriorityBox>
+
+                </Options>
+                <Options>
+                    <TextOptions>Deadline</TextOptions>
+                    <Text>12/05/23</Text>
+                </Options>
+                <IconsBox>
+                    <TransferRight>
+                        <use href={icon + '#icon-arrow-circle-broken-right'}></use>
+                    </TransferRight>
+                    <Edit>
+                        <use href={icon + '#icon-pencil-01'}></use>
+                    </Edit>
+                    <Delete>
+                        <use href={icon + '#icon-trash-04'}></use>
+                    </Delete>
+                </IconsBox>
+            </BottomBar>
+            
         </Container>
     );
 };
--- src/components/ScreensPage/card/Card.styled.jsx
+++ src/components/ScreensPage/card/Card.styled.jsx
@@ -23,4 +23,65 @@ font-size: 12px;
 font-style: normal;
 font-weight: 400;
 margin-top: 8px;
+margin-bottom: 14px;
+`
+export const BottomBar = styled.div`
+margin-top: 14px;
+border-top: 1px solid rgba(22, 22, 22, 0.10);
+padding-top: 14px;
+display: flex;
+align-items: flex-end;
+`
+
+export const Options = styled.div`
+margin-right: 14px;
+`
+export const PriorityBox = styled.div`
+display: flex;
+align-items: center;
+`
+
+export const TextOptions = styled.p`
+color: rgba(22, 22, 22, 0.50);
+font-family: Poppins;
+font-size: 8px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+`
+export const Text = styled.div`
+color: #161616;
+font-family: Poppins;
+font-size: 10px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+`
+
+export const Elipce = styled.div`
+border-radius: 100%;
+width: 12px;
+height: 12px;
+margin-right: 4px;
+`
+export const IconsBox = styled.div`
+display: flex;
+// background:tomato;
+margin-left: 112px;
+`
+export const TransferRight = styled.svg`
+width: 16px;
+height: 16px;
+margin-right: 8px;
+`
+export const Edit = styled.svg`
+width: 16px;
+height: 16px;
+color:#16161680;
+margin-right: 8px;
+`
+export const Delete = styled.svg`
+width: 16px;
+height: 16px;
+color:#16161680;
 `
