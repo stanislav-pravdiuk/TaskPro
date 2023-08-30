c7d858d7a3a73a3a94422e36d3110616459149ff
stanislav-pravdiuk <114864904+stanislav-pravdiuk@users.noreply.github.com>
2023-08-30 09:27:30 +0000

    Deploying to gh-pages from @ stanislav-pravdiuk/TaskPro@1d48167e1ac282c1e22b4680dd52811fd771817e 🚀

--- static/js/780.73b4dc29.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktask_pro=self.webpackChunktask_pro||[]).push([[780],{5780:function(s,c,a){a(2791);c.Z=a.p+"static/media/icon.636fb0c87360629bdb2addc554ed2b48.svg"}}]);
-//# sourceMappingURL=780.73b4dc29.chunk.js.map
