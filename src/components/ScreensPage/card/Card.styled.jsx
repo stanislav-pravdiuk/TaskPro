df743a7343972ee84126f084b12f0f8670829750
Ihor Karen <karenihor@gmail.com>
2023-08-30 16:27:27 +0300

    delete columns/cards

--- src/components/ScreensPage/card/Card.styled.jsx
+++ src/components/ScreensPage/card/Card.styled.jsx
@@ -1,92 +1,92 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-width: 334px;
-height: auto;
-border-radius: 8px;
-background: #FFF;
-padding: 14px 20px;
-margin-bottom: 8px;
-`
+  width: 334px;
+  height: auto;
+  border-radius: 8px;
+  background: #fff;
+  padding: 14px 20px;
+  margin-bottom: 8px;
+`;
 export const Title = styled.h4`
-color: #161616;
-font-family: Poppins;
-font-size: 14px;
-font-style: normal;
-font-weight: 600;
-line-height: normal;
-margin: 0;
-`
+  color: #161616;
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin: 0;
+`;
 export const Description = styled.div`
-color: rgba(22, 22, 22, 0.70);
-font-family: Poppins;
-font-size: 12px;
-font-style: normal;
-font-weight: 400;
-margin-top: 8px;
-margin-bottom: 14px;
-`
+  color: rgba(22, 22, 22, 0.7);
+  font-family: Poppins;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  margin-top: 8px;
+  margin-bottom: 14px;
+`;
 export const BottomBar = styled.div`
-margin-top: 14px;
-border-top: 1px solid rgba(22, 22, 22, 0.10);
-padding-top: 14px;
-display: flex;
-align-items: flex-end;
-justify-content: space-between;
-`
+  margin-top: 14px;
+  border-top: 1px solid rgba(22, 22, 22, 0.1);
+  padding-top: 14px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
 export const OptionsBox = styled.div`
-display:flex;
-`
+  display: flex;
+`;
 
 export const Options = styled.div`
-margin-right: 14px;
-`
+  margin-right: 14px;
+`;
 export const PriorityBox = styled.div`
-display: flex;
-align-items: center;
-`
+  display: flex;
+  align-items: center;
+`;
 
 export const TextOptions = styled.p`
-color: rgba(22, 22, 22, 0.50);
-font-family: Poppins;
-font-size: 8px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-`
+  color: rgba(22, 22, 22, 0.5);
+  font-family: Poppins;
+  font-size: 8px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
 export const Text = styled.div`
-color: #161616;
-font-family: Poppins;
-font-size: 10px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-`
+  color: #161616;
+  font-family: Poppins;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
 
 export const Elipce = styled.div`
-border-radius: 100%;
-width: 12px;
-height: 12px;
-margin-right: 4px;
-`
+  border-radius: 100%;
+  width: 12px;
+  height: 12px;
+  margin-right: 4px;
+`;
 export const IconsBox = styled.div`
-display: flex;
-// background:tomato;
-// margin-left: 112px;
-`
+  display: flex;
+  // background:tomato;
+  // margin-left: 112px;
+`;
 export const TransferRight = styled.svg`
-width: 16px;
-height: 16px;
-margin-right: 8px;
-`
+  width: 16px;
+  height: 16px;
+  margin-right: 8px;
+`;
 export const Edit = styled.svg`
-width: 16px;
-height: 16px;
-color:#16161680;
-margin-right: 8px;
-`
+  width: 16px;
+  height: 16px;
+  color: #16161680;
+  margin-right: 8px;
+`;
 export const Delete = styled.svg`
-width: 16px;
-height: 16px;
-color:#16161680;
-`
+  width: 16px;
+  height: 16px;
+  color: #16161680;
+`;
