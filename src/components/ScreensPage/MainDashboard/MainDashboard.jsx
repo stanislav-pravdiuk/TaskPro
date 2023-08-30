0de3e010ad287d1767537eb64df4655e3ad557b4
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-08-30 06:46:50 +0300

    Update MainDashboard.jsx

--- src/components/ScreensPage/MainDashboard/MainDashboard.jsx
+++ src/components/ScreensPage/MainDashboard/MainDashboard.jsx
@@ -4,32 +4,32 @@ import { Container, Column, GalleryCards } from './MainDashboard.styled';
 import BtnAdd from '../btnAdd/BtnAdd';
 
 const MainDashboard = ({ columns }) => {
-  return (
-    <Container>
-      {columns &&
-        columns.map(column => {
-          return (
-            <Column key={column._id}>
-              <TitleColumn title={column.title} />
-              <GalleryCards>
-                {column.cards.map(card => {
-                  return (
-                    <Card
-                      key={card._id}
-                      title={card.title}
-                      text={card.text}
-                      priority={card.priority}
-                      deadline={card.deadline}
-                    />
-                  );
+    return (
+        <Container>
+            {columns &&
+                columns.map(column => {
+                    return (
+                        <Column key={column._id}>
+                            <TitleColumn title={column.title} />
+                            <GalleryCards>
+                                {column.cards.map(card => {
+                                    return (
+                                        <Card
+                                            key={card._id}
+                                            title={card.title}
+                                            text={card.text}
+                                            priority={card.priority}
+                                            deadline={card.deadline}
+                                        />
+                                    );
+                                })}
+                            </GalleryCards>
+                            <BtnAdd />
+                        </Column>
+                    );
                 })}
-              </GalleryCards>
-              <BtnAdd />
-            </Column>
-          );
-        })}
-    </Container>
-  );
+        </Container>
+    );
 };
 
 export default MainDashboard;
