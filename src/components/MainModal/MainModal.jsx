21f199dfff504f592096a94c3663c684e8b70e45
Ihor Karen <karenihor@gmail.com>
2023-09-01 02:04:28 +0300

    modal error fix

--- src/components/MainModal/MainModal.jsx
+++ src/components/MainModal/MainModal.jsx
@@ -23,7 +23,7 @@ const MainModal = ({ modalIsOpen, closeModal, children }) => {
         boxShadow: '0 1px 3px 0 rgba(0, 0, 0, 0.1)',
       }}
     >
-      {children}
+      <>{children}</>
     </Modal>
   );
 };
