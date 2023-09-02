a6c696e60fb4808cc35a788fa60eb4675d0a485b
Anna Kravets <annakravets1906@gmail.com>
2023-09-02 21:57:24 +0300

    Added Formik

--- src/components/newBoardForm/NewBoardForm.jsx
+++ src/components/newBoardForm/NewBoardForm.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Formik } from 'formik';
+
 import images from '../iconSvg/images.js';
-// import bgImage from '../../images/block.png';
 import bgImage from '../iconSvg/bgImage.svg';
 
 import sprite from '../iconSvg/icon.svg';
@@ -15,42 +15,36 @@ import {
   BgColor,
   IconList,
   BgList,
-  RadioButton,
+  RadioField,
+  RadioFieldBg,
+  FormikContainer,
 } from './NewBoardForm.styled';
 import ModalClose from '@mui/joy/ModalClose';
 import BtnAdd from 'components/ScreensPage/btnAdd/BtnAdd';
 
 const NewBoardForm = ({ formTitle, boardTitle, btnText, handleSubmit }) => {
-  const [title, setTitle] = useState();
-  const [icon, setIcon] = useState();
   const [background, setbackground] = useState();
+  console.log(background);
 
-  const inputTitleHandler = e => {
-    setTitle(e.target.value);
+  const formSubmit = values => {
+    const data = { ...values, background };
+    console.log(data);
+
+    handleSubmit(data, formTitle);
   };
 
-  const IconChangeHandler = e => {
-    setIcon(`#${e.target.value}`);
+  const initialValues = {
+    title: 'gvgrfg',
+    icon: '#icon-Project',
+    background: '',
   };
 
   const BgImageChangeHandler = data => {
     setbackground(data);
   };
 
-  const formSubmit = e => {
-    e.preventDefault();
-
-    const data = { title, icon, background };
-
-    handleSubmit(data, formTitle);
-  };
-
-  useEffect(() => {
-    setTitle(boardTitle || '');
-  }, [boardTitle]);
-
   return (
-    <FormContainer onSubmit={formSubmit}>
+    <FormContainer>
       <ModalClose
         sx={{
           position: 'absolute',
@@ -60,153 +54,119 @@ const NewBoardForm = ({ formTitle, boardTitle, btnText, handleSubmit }) => {
         }}
       />
       <Title>{formTitle}</Title>
-      <Input
-        type="text"
-        placeholder="Title"
-        name="title"
-        value={title}
-        onChange={e => inputTitleHandler(e)}
-      />
+      <Formik initialValues={initialValues} onSubmit={formSubmit}>
+        <FormikContainer>
+          <Input type="text" placeholder="Title" name="title" />
 
-      <Text>Icons</Text>
-      <IconList>
-        <li>
-          <label>
-            <RadioButton
-              type="radio"
-              name="icon"
-              value="icon-Project"
-              onChange={e => IconChangeHandler(e)}
-            />
-            <Icon>
-              <use href={sprite + '#icon-Project'}></use>
-            </Icon>
-          </label>
-        </li>
-        <li>
-          <label>
-            <RadioButton
-              type="radio"
-              name="icon"
-              value="icon-star-04"
-              onChange={e => IconChangeHandler(e)}
-            />
-            <Icon>
-              <use href={sprite + '#icon-star-04'}></use>
-            </Icon>
-          </label>
-        </li>
-        <li>
-          <label>
-            <RadioButton
-              type="radio"
-              name="icon"
-              value="icon-loading-03"
-              onChange={e => IconChangeHandler(e)}
-            />
-            <Icon>
-              <use href={sprite + '#icon-loading-03'}></use>
-            </Icon>
-          </label>
-        </li>
-        <li>
-          <label>
-            <RadioButton
-              type="radio"
-              name="icon"
-              value="icon-puzzle-piece-02"
-              onChange={e => IconChangeHandler(e)}
-            />
-            <Icon>
-              <use href={sprite + '#icon-puzzle-piece-02'}></use>
-            </Icon>
-          </label>
-        </li>
-        <li>
-          <label>
-            <RadioButton
-              type="radio"
-              name="icon"
-              value="icon-container"
-              onChange={e => IconChangeHandler(e)}
-            />
-            <Icon>
-              <use href={sprite + '#icon-container'}></use>
-            </Icon>
-          </label>
-        </li>
-        <li>
-          <label>
-            <RadioButton
-              type="radio"
-              name="icon"
-              value="icon-lightning-02"
-              onChange={e => IconChangeHandler(e)}
-            />
-            <Icon>
-              <use href={sprite + '#icon-lightning-02'}></use>
-            </Icon>
-          </label>
-        </li>
-        <li>
-          <label>
-            <RadioButton
-              type="radio"
-              name="icon"
-              value="icon-colors"
-              onChange={e => IconChangeHandler(e)}
-            />
-            <Icon>
-              <use href={sprite + '#icon-colors'}></use>
-            </Icon>
-          </label>
-        </li>
-        <li>
-          <label>
-            <RadioButton
-              type="radio"
-              name="icon"
-              value="icon-hexagon-01"
-              onChange={e => IconChangeHandler(e)}
-            />
-            <Icon>
-              <use href={sprite + '#icon-hexagon-01'}></use>
-            </Icon>
-          </label>
-        </li>
-      </IconList>
+          <Text>Icons</Text>
+          <IconList>
+            <li>
+              <label>
+                <RadioField type="radio" name="icon" value="icon-Project" />
+                <Icon>
+                  <use href={sprite + '#icon-Project'}></use>
+                </Icon>
+              </label>
+            </li>
+            <li>
+              <label>
+                <RadioField type="radio" name="icon" value="icon-star-04" />
+                <Icon>
+                  <use href={sprite + '#icon-star-04'}></use>
+                </Icon>
+              </label>
+            </li>
+            <li>
+              <label>
+                <RadioField type="radio" name="icon" value="icon-loading-03" />
+                <Icon>
+                  <use href={sprite + '#icon-loading-03'}></use>
+                </Icon>
+              </label>
+            </li>
+            <li>
+              <label>
+                <RadioField
+                  type="radio"
+                  name="icon"
+                  value="icon-puzzle-piece-02"
+                />
+                <Icon>
+                  <use href={sprite + '#icon-puzzle-piece-02'}></use>
+                </Icon>
+              </label>
+            </li>
+            <li>
+              <label>
+                <RadioField type="radio" name="icon" value="icon-container" />
+                <Icon>
+                  <use href={sprite + '#icon-container'}></use>
+                </Icon>
+              </label>
+            </li>
+            <li>
+              <label>
+                <RadioField
+                  type="radio"
+                  name="icon"
+                  value="icon-lightning-02"
+                />
+                <Icon>
+                  <use href={sprite + '#icon-lightning-02'}></use>
+                </Icon>
+              </label>
+            </li>
+            <li>
+              <label>
+                <RadioField type="radio" name="icon" value="icon-colors" />
+                <Icon>
+                  <use href={sprite + '#icon-colors'}></use>
+                </Icon>
+              </label>
+            </li>
+            <li>
+              <label>
+                <RadioField type="radio" name="icon" value="icon-hexagon-01" />
+                <Icon>
+                  <use href={sprite + '#icon-hexagon-01'}></use>
+                </Icon>
+              </label>
+            </li>
+          </IconList>
 
-      <Text>Background</Text>
-      <BgList>
-        <BgColor>
-          <label>
-            <RadioButton
-              type="radio"
-              name="bgImage"
-              onChange={() => BgImageChangeHandler(null)}
-            />
-            <Icon>
-              <use href={bgImage + 'icon-block'}></use>
-            </Icon>
-            {/* <img src={bgImage} alt="bgImage" /> */}
-          </label>
-        </BgColor>
-        {images.map(image => (
-          <BgColor key={image.min}>
-            <label>
-              <RadioButton
-                type="radio"
-                name="bgImage"
-                onChange={() => BgImageChangeHandler(image)}
-              />
-              <Img src={image.min} alt="bgImage" />
-            </label>
-          </BgColor>
-        ))}
-      </BgList>
-      <BtnAdd btnTitle={btnText} btnColor={'#BEDBB0'} />
+          <Text>Background</Text>
+          <BgList>
+            <BgColor>
+              <label>
+                <RadioFieldBg type="radio" name="background" value="" />
+                <Icon>
+                  <use href={bgImage + '#icon-block'}></use>
+                </Icon>
+                {/* <img src={bgImage} alt="bgImage" /> */}
+              </label>
+            </BgColor>
+            {images.map(image => (
+              <BgColor key={image.min}>
+                <label>
+                  <RadioFieldBg
+                    type="radio"
+                    name="background"
+                    onChange={() => BgImageChangeHandler(image)}
+                  />
+                  <Img src={image.min} alt="bgImage" />
+                </label>
+              </BgColor>
+            ))}
+          </BgList>
+          <BtnAdd btnTitle={btnText} btnColor={'#BEDBB0'} />
+        </FormikContainer>
+      </Formik>
     </FormContainer>
   );
 };
 
 export default NewBoardForm;
+
+//  useEffect(() => {
+//    setTitle(boardTitle || '');
+//  }, [boardTitle]);
