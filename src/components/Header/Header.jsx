577fde209152776749d9b326311beba8ce0f3961
Anastasiia Pastukhova <nastapastuhova778@gmail.com>
2023-09-03 16:20:48 +0300

    fix styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import icon from '../../components/iconSvg/icon.svg';
-import { Box, Button, Typography } from '@mui/material';
-import { BurgerIcon, UserImage, UserIcon } from './Header.styled';
+import {
+  BurgerIcon,
+  UserImage,
+  UserIcon,
+  Appbar,
+  ToolBar,
+  BurgerButton,
+  HeaderWrap,
+  UserText,
+  UserButton,
+} from './Header.styled';
 import { selectUser } from 'redux/auth/authSelectors';
 import { useSelector } from 'react-redux';
 import ProfileEditModal from 'components/profileEditModal/ProfileEditModal';
@@ -31,77 +38,17 @@ const Header = ({ click }) => {
   };
 
   return (
-    <AppBar
-      position="relative"
-      sx={{ width: '100%', backgroundColor: '#FCFCFC', boxShadow: 'none' }}
-    >
-      <Toolbar
-        sx={{
-          paddingRight: '32px',
-          paddingLeft: '32px',
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Button
-          aria-label="open drawer"
-          edge="start"
-          onClick={click}
-          sx={{
-            '@media (min-width: 1440px)': {
-              display: { xs: 'block', sm: 'none' },
-            },
-            padding: 0,
-            minWidth: 0,
-            '&:hover': { backgroundColor: 'transparent' },
-          }}
-        >
+    <Appbar>
+      <ToolBar>
+        <BurgerButton aria-label="open drawer" edge="start" onClick={click}>
           <BurgerIcon>
             <use href={icon + '#icon-menu_40px-1'}></use>
           </BurgerIcon>
-        </Button>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'end',
-            width: '100%',
-            alignItems: 'center',
-          }}
-        >
-          <Box
-            sx={{
-              marginRight: '30px',
-            }}
-          ></Box>
+        </BurgerButton>
+        <HeaderWrap>
           <ThemeComponent />
-          <Typography
-            variant="body2"
-            sx={{
-              color: 'rgba(22, 22, 22, 1)',
-              fontFamily: 'Poppins',
-              width: 'auto',
-              fontWeight: 500,
-              fontSize: '14px',
-              lineHeight: 0.7,
-              marginRight: '8px',
-            }}
-          >
-            {user.name}
-          </Typography>
-          <Button
-            sx={{
-              padding: 0,
-              minWidth: 0,
-              border: 0,
-              '&:hover': {
-                backgroundColor: 'inherit',
-                border: 0,
-                transform: 'scale(1.1)',
-              },
-            }}
-            onClick={openModal}
-            aria-label="open drawer"
-          >
+          <UserText variant="body2">{user.name}</UserText>
+          <UserButton onClick={openModal} aria-label="open drawer">
             {currentUserData ? (
               <UserImage src={currentUserData.avatar} alt="User avatar" />
             ) : (
@@ -109,13 +56,13 @@ const Header = ({ click }) => {
                 <use href={icon + '#icon-user-1'}></use>
               </UserIcon>
             )}
-          </Button>
+          </UserButton>
           <MainModal modalIsOpen={isModalOpen} closeModal={closeModal}>
             <ProfileEditModal user={currentUserData} modalClose={closeModal} />
           </MainModal>
-        </Box>
-      </Toolbar>
-    </AppBar>
+        </HeaderWrap>
+      </ToolBar>
+    </Appbar>
   );
 };
 
