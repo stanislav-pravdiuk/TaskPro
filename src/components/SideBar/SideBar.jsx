723a83ea8e4d377fe97f2d14d4ceb501854a6b84
Ihor Karen <karenihor@gmail.com>
2023-09-03 18:34:51 +0300

    rollback

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Box, Drawer } from '@mui/material';
+import { Box, Button, Typography, Drawer, Link } from '@mui/material';
 import cactus from '../../images/cactus.png';
 import cactus2x from '../../images/cactus@2x.png';
 import cactus3x from '../../images/cactus@3x.png';
@@ -23,28 +23,11 @@ import {
   Title,
   IconButton,
   IconLink,
-  LogoText,
-  Subtitle,
-  Wrap,
-  LogoWrap,
-  Text,
-  PlusButton,
-  HelpWrap,
-  ImgWrap,
-  TextWrap,
-  HelpText,
-  HelpTextLink,
-  HelpButton,
-  HelpTitle,
-  LogoutWrap,
-  LogoutButton,
-  LogoutText,
 } from './Sidebar.styled';
 import { useGetBoardsQuery } from 'redux/boards/boardsApi';
 import { useLocation, useParams } from 'react-router-dom';
 
 import NewBoardForm from 'components/newBoardForm/NewBoardForm';
-import ModalHelp from 'components/ModalHelp/ModalHelp';
 import MainModal from 'components/MainModal/MainModal';
 import NeedHelpModal from 'components/ModalHelp/ModalHelp';
 import sprite from '../iconSvg/icon.svg';
@@ -60,10 +43,9 @@ import { logOut } from 'redux/auth/authOperations';
 const SideBar = ({ active, onClick }) => {
   const [openAddModal, setOpenAddModal] = useState(false);
   const [openEditModal, setOpenEditModal] = useState(false);
-  const [openHelpModal, setOpenHelpModal] = useState(false);
   const [activeBoardTitle, setActiveBoardTitle] = useState('');
   const [activeBoardIcon, setActiveBoardIcon] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const [isModalOpen, setIsModalOpen] = useState(false);
   const { data = [] } = useGetBoardsQuery();
 
   const location = useLocation();
@@ -74,14 +56,14 @@ const SideBar = ({ active, onClick }) => {
   const [deleteBoard] = useDeleteBoardMutation();
   const dispatch = useDispatch();
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+ const openModal = () => {
+   setIsModalOpen(true);
+ };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
+ const closeModal = () => {
+   setIsModalOpen(false);
   };
-
+  
   const openEditModalHandler = (boardName, boardIcon) => {
     setActiveBoardTitle(boardName);
     setActiveBoardIcon(boardIcon);
@@ -94,12 +76,6 @@ const SideBar = ({ active, onClick }) => {
 
   const closeEditModal = () => {
     setOpenEditModal(false);
-  };
-
-  const closeHelpModal = () => {
-    console.log(openAddModal);
-    console.log(openHelpModal);
-    setOpenHelpModal(false);
   };
 
   const handleSubmit = (data, formTitle) => {
@@ -124,21 +100,80 @@ const SideBar = ({ active, onClick }) => {
 
   const drawerContent = (
     <SideBarStyled>
-      <LogoWrap>
+      <Box
+        sx={{
+          display: 'flex',
+          gap: '8px',
+          alignItems: 'center',
+          marginBottom: '60px',
+        }}
+      >
         <LogoIcon>
           <use href={icon + '#icon-icon-1'}></use>
         </LogoIcon>
-        <LogoText variant="h2">Task Pro</LogoText>
-      </LogoWrap>
-      <Subtitle variant="subtitle1">My boards</Subtitle>
-      <Wrap>
-        <Text variant="body2">Create a new board</Text>
-        <PlusButton onClick={() => setOpenAddModal(true)}>
+        <Typography
+          variant="h2"
+          sx={{
+            fontSize: '16px',
+            letterSpacing: 0.7,
+            fontWeight: 600,
+            color: '#161616',
+          }}
+        >
+          Task Pro
+        </Typography>
+      </Box>
+      <Typography
+        variant="subtitle1"
+        sx={{
+          fontSize: '12px',
+          letterSpacing: 0.7,
+          fontWeight: 400,
+          color: 'rgba(22, 22, 22, 0.5)',
+        }}
+      >
+        My boards
+      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          borderBottom: '1px solid rgba(22, 22, 22, 0.1)',
+          borderTop: '1px solid rgba(22, 22, 22, 0.1)',
+          padding: '14px 0',
+          marginTop: '8px',
+          marginBottom: '40px',
+        }}
+      >
+        <Typography
+          variant="body2"
+          sx={{
+            maxWidth: '76px',
+            fontWeight: 500,
+            fontSize: '14px',
+            letterSpacing: 0.7,
+          }}
+        >
+          Create a new board
+        </Typography>
+        <Button
+          onClick={() => setOpenAddModal(true)}
+          sx={{
+            backgroundColor: '#BEDBB0',
+            padding: '8px 10px',
+            minWidth: 0,
+            '&:hover': {
+              backgroundColor: '#BEDBB0',
+              transform: 'scale(1.1)',
+            },
+          }}
+        >
           <PlusIcon>
             <use href={icon + '#icon-plus-2'}></use>
           </PlusIcon>
-        </PlusButton>
-      </Wrap>
+        </Button>
+      </Box>
       <BoardsContainer>
         <BoardsList>
           {data &&
@@ -186,39 +221,134 @@ const SideBar = ({ active, onClick }) => {
             })}
         </BoardsList>
       </BoardsContainer>
-      <HelpWrap>
-        <ImgWrap>
+      <Box
+        sx={{
+          backgroundColor: 'rgba(246, 246, 247, 1)',
+          marginTop: 'calc(100vh - 575px)',
+          borderRadius: '8px',
+          padding: '20px',
+        }}
+      >
+        <Box
+          sx={{
+            marginBottom: '14px',
+          }}
+        >
           <picture>
             <source srcSet={`${cactus} 1x, ${cactus2x} 2x, ${cactus3x} 3x`} />
             <img srcSet={`${cactus} 1x`} alt="cactus" />
           </picture>
-        </ImgWrap>
-        <TextWrap>
-          <HelpText variant="body2">
+        </Box>
+        <Box
+          sx={{
+            marginBottom: '18px',
+          }}
+        >
+          <Typography
+            variant="body2"
+            sx={{
+              fontWeight: 400,
+              fontSize: '14px',
+              letterSpacing: 0.7,
+              color: 'rgba(22, 22, 22, 1)',
+            }}
+          >
             If you need help with
-
-            <HelpTextLink onClick={openModal}> TaskPro</HelpTextLink>, check out
-            our support resources or reach out to our customer support team.
-          </HelpText>
-        </TextWrap>
-         onClick={() => setOpenHelpModal(true)}
+            <Link
+              sx={{
+                fontWeight: 400,
+                fontSize: '14px',
+                letterSpacing: 0.7,
+                color: 'rgba(190, 219, 176, 1)',
+                textDecoration: 'none',
+              }}
+              onClick={openModal}
+            >
+              {' '}
+              TaskPro
+            </Link>
+            , check out our support resources or reach out to our customer
+            support team.
+          </Typography>
+        </Box>
+        <Button
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px',
+            padding: 0,
+            minWidth: 0,
+            border: 0,
+            '&:hover': {
+              backgroundColor: 'inherit',
+              border: 0,
+              transform: 'scale(1.1)',
+            },
+          }}
+          onClick={openModal}
+        >
           <HelpIcon>
             <use href={icon + '#icon-help'}></use>
           </HelpIcon>
-          <HelpTitle variant="body2">Need help?</HelpTitle>
-        </HelpButton>
+          <Typography
+            sx={{
+              color: 'rgba(22, 22, 22, 1)',
+              textTransform: 'none',
+              fontWeight: 500,
+              fontSize: '12px',
+              letterSpacing: 0.7,
+            }}
+            variant="body2"
+          >
+            Need help?
+          </Typography>
+        </Button>
         <MainModal modalIsOpen={isModalOpen} closeModal={closeModal}>
           <NeedHelpModal closeModal={closeModal} />
         </MainModal>
-      </HelpWrap>
-      <LogoutWrap>
-        <LogoutButton onClick={() => dispatch(logOut())}>
+      </Box>
+      <Box
+        sx={{
+          marginTop: '24px',
+          textTransform: 'none',
+          fontWeight: 500,
+          fontSize: '12px',
+          letterSpacing: 0.7,
+        }}
+      >
+        <Button
+          onClick={() => dispatch(logOut())}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '14px',
+            padding: 0,
+            minWidth: 0,
+            border: 0,
+            '&:hover': {
+              backgroundColor: 'inherit',
+              border: 0,
+              transform: 'scale(1.1)',
+            },
+          }}
+        >
           <LogoutIcon>
             <use href={icon + '#icon-login'}></use>
           </LogoutIcon>
-          <LogoutText variant="body2">Log out</LogoutText>
-        </LogoutButton>
-      </LogoutWrap>
+          <Typography
+            sx={{
+              color: 'rgba(22, 22, 22, 1)',
+              textTransform: 'none',
+              fontWeight: 500,
+              fontSize: '16px',
+              letterSpacing: 0.7,
+            }}
+            variant="body2"
+          >
+            Log out
+          </Typography>
+        </Button>
+      </Box>
     </SideBarStyled>
   );
 
@@ -292,12 +422,6 @@ const SideBar = ({ active, onClick }) => {
           boardTitle={activeBoardTitle}
           boardIcon={activeBoardIcon}
         ></NewBoardForm>
-      </MainModal>
-      <MainModal modalIsOpen={openHelpModal} closeModal={closeHelpModal}>
-        <ModalHelp />
-      </MainModal>
-      <MainModal modalIsOpen={isModalOpen} closeModal={closeModal}>
-        <NeedHelpModal closeModal={closeModal} />
       </MainModal>
     </Box>
   );
