295bc3214abdd472c749d57c8004b5688e828cce
Ihor Karen <karenihor@gmail.com>
2023-09-06 14:37:03 +0300

    Team grid

--- src/components/teamGallery/TeamGallery.styled.jsx
+++ src/components/teamGallery/TeamGallery.styled.jsx
@@ -2,73 +2,75 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 1200px;
+  max-width: 900px;
   margin: 0 auto;
   padding: 20px;
   background-color: opacity;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  display: flex;
+  flex-wrap: wrap;
   gap: 20px;
-  justify-items: center;
+  justify-content: center;
 `;
 
 export const Card = styled.div`
   display: flex;
+  flex-shrink: 0;
   flex-direction: column;
   align-items: center;
   border-radius: 8px;
-  background: #FFF;
+  background: #fff;
   padding: 18px;
   width: 184px;
 `;
 export const UserPhotoBox = styled.div`
-width: 68px;
-height: 68px;
-margin-bottom:14px;
-border-radius: 8px;
-overflow: hidden;
+  width: 68px;
+  height: 68px;
+  margin-bottom: 14px;
+  border-radius: 8px;
+  overflow: hidden;
 `;
 export const UserPhoto = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: cover;`
+  object-fit: cover;
+`;
 
 export const NameBox = styled.div`
-display: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
-  margin-bottom:14px;`
+  margin-bottom: 14px;
+`;
 export const Name = styled.h3`
-color: var(--color-text-dark-main);
-font-family: Poppins;
-font-size: 14px;
-font-style: normal;
-font-weight: 600;
-line-height: normal;
+  color: var(--color-text-dark-main);
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
 `;
 export const Position = styled.p`
-color: var(--color-text-dark-07);
-text-overflow: ellipsis;
-whitespace: nowrap;
-font-family: Poppins;
-font-size: 12px;
-font-style: normal;
-font-weight: 400;
-text-align:center;
+  color: var(--color-text-dark-07);
+  text-overflow: ellipsis;
+  whitespace: nowrap;
+  font-family: Poppins;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  text-align: center;
 `;
 export const LinkBox = styled.div`
-display: flex;
-justify-content: space-around;
+  display: flex;
+  justify-content: space-around;
   align-items: center;
-  `;
+`;
 
 export const Link = styled.a`
-margin: 0 8px;
+  margin: 0 8px;
 `;
 
 export const IconLink = styled.svg`
-width: 16px;
-height: 16px;
-fill: var(--color-text-dark);
+  width: 16px;
+  height: 16px;
+  fill: var(--color-text-dark);
 `;
-
