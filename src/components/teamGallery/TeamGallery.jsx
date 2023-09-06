d4b6e7a2a36d2cfb40c753257b1105f2095b1692
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-09-06 11:54:36 +0300

    save
    sample

--- src/components/teamGallery/TeamGallery.jsx
+++ src/components/teamGallery/TeamGallery.jsx
@@ -1,23 +1,25 @@
-import { Card, Container, LinkBox, Name, NameBox, Position, Title, UserPhoto } from "./TeamGallery.styled";
-
+import { Card, Container, IconLink, Link, LinkBox, Name, NameBox, Position, Title, UserPhoto, UserPhotoBox } from "./TeamGallery.styled";
+import icon from '../iconSvg/icon.svg'
 
 function TeamGallery() { 
     return (
         <Container>
             <Title>Our Team</Title>
             <Card>
-                <UserPhoto>
-                    <img src="../../images/developers/StanislavPravdiuk.jpg"
-                    alt=""
-                    />
-                </UserPhoto>
+                <UserPhotoBox>
+                    <UserPhoto src="https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+                </UserPhotoBox>
                 <NameBox>
-                    <Name>Name</Name>
-                    <Position>Position</Position>
+                    <Name>Stanislav Pravdiuk</Name>
+                    <Position>Team Lead [Frontend]</Position>
                 </NameBox>
                 <LinkBox>
-                    <a href="#">icon or text GitHub</a>
-                    <a href="#"> icon or text Telegram</a>
+                    <Link href="https://github.com/stanislav-pravdiuk">
+                        <IconLink><use href={icon + '#icon-gitHub'}></use></IconLink>
+                    </Link>
+                    <Link href="https://t.me/Pravdiuk_Stanislav">
+                        <IconLink><use href={icon + '#icon-telegram'}></use></IconLink>
+                    </Link>
                 </LinkBox>
             </Card>
         </Container>
--- src/components/teamGallery/TeamGallery.styled.jsx
+++ src/components/teamGallery/TeamGallery.styled.jsx
@@ -1,10 +1,82 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
-export const Title = styled.h2``;
-export const Card = styled.div``;
-export const UserPhoto = styled.div``;
-export const NameBox = styled.div``;
-export const Name = styled.h3``;
-export const Position = styled.p``;
-export const LinkBox = styled.div``;
+export const Container = styled.div`
+ width: 100%;
+  max-width:
+  margin: 0 auto;
+  padding: 20px;
+  background-color: opacity;
+`;
+export const Title = styled.h2`
+color: var(--color-text-dark-main);
+font-family: Poppins;
+font-size: 18px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+text-align: center;
+  border-radius: 8px;
+  background: #FFF;
+  padding: 18px;
+  margin-bottom: 14px;
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 8px;
+  background: #FFF;
+  padding: 18px;
+  width: 174px;
+`;
+export const UserPhotoBox = styled.div`
+width: 68px;
+height: 68px;
+margin-bottom:14px;
+border-radius: 8px;
+overflow: hidden;
+`;
+export const UserPhoto = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;`
+
+export const NameBox = styled.div`
+display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom:14px;`
+export const Name = styled.h3`
+color: var(--color-text-dark-main);
+font-family: Poppins;
+font-size: 14px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
+`;
+export const Position = styled.p`
+color: var(--color-text-dark-07);
+text-overflow: ellipsis;
+whitespace: nowrap;
+font-family: Poppins;
+font-size: 12px;
+font-style: normal;
+font-weight: 400;
+`;
+export const LinkBox = styled.div`
+display: flex;
+justify-content: space-around;
+  align-items: center;
+  `;
+
+export const Link = styled.a`
+margin: 0 8px;
+`;
+
+export const IconLink = styled.svg`
+width: 16px;
+height: 16px;
+fill: var(--color-text-dark);
+`;
+
