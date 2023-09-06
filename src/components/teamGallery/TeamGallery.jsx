22e23ef8fe7645340d5576b618abbc2667d3a35f
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-09-06 14:48:30 +0300

    add
    OksanaPelekhata

--- src/components/teamGallery/TeamGallery.jsx
+++ src/components/teamGallery/TeamGallery.jsx
@@ -100,14 +100,14 @@ function TeamGallery() {
                         <UserPhoto src="https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
                     </UserPhotoBox>
                     <NameBox>
-                        <Name>Оксана Пелехата</Name>
-                        <Position>Developer [Backend/Frontend]</Position>
+                        <Name>Oksana Pelekhata</Name>
+                        <Position>Developer [Frontend]</Position>
                     </NameBox>
                     <LinkBox>
-                        <Link href="https://github.com/#">
+                        <Link href="https://github.com/Oksana-Pelekhata">
                             <IconLink><use href={icon + '#icon-gitHub'}></use></IconLink>
                         </Link>
-                        <Link href="https://t.me/#">
+                        <Link href="https://t.me/OksanaPelekhata">
                             <IconLink><use href={icon + '#icon-telegram'}></use></IconLink>
                         </Link>
                     </LinkBox>
