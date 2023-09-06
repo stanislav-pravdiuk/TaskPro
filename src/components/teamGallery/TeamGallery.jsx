8a5609111d68c437e1334c0d754f2a7a19f13c6e
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-09-06 14:30:53 +0300

    add
    SORO_light

--- src/components/teamGallery/TeamGallery.jsx
+++ src/components/teamGallery/TeamGallery.jsx
@@ -190,14 +190,14 @@ function TeamGallery() {
                         <UserPhoto src="https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
                     </UserPhotoBox>
                     <NameBox>
-                        <Name>Світлана Сорока</Name>
-                        <Position>Developer [Backend/Frontend]</Position>
+                        <Name>Svitlana Soroka</Name>
+                        <Position>Developer [Frontend]</Position>
                     </NameBox>
                     <LinkBox>
-                        <Link href="https://github.com/#">
+                        <Link href="https://github.com/SorokaSvitlana">
                             <IconLink><use href={icon + '#icon-gitHub'}></use></IconLink>
                         </Link>
-                        <Link href="https://t.me/#">
+                        <Link href="https://t.me/SORO_light">
                             <IconLink><use href={icon + '#icon-telegram'}></use></IconLink>
                         </Link>
                     </LinkBox>
