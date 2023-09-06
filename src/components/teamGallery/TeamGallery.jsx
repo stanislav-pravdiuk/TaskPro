20569e8ad6a680a0900bda06696fffc484d10825
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-09-06 14:46:54 +0300

    add
    @Darekas97

--- src/components/teamGallery/TeamGallery.jsx
+++ src/components/teamGallery/TeamGallery.jsx
@@ -61,11 +61,11 @@ function TeamGallery() {
 
                 <Card>
                     <UserPhotoBox>
-                        <UserPhoto src="https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+                        <UserPhoto src="../../images/developers/KravetsHanna.jpg" alt="" />
                     </UserPhotoBox>
                     <NameBox>
                         <Name>Kravets Hanna</Name>
-                        <Position>Developer [Backend/Frontend]</Position>
+                        <Position>Developer [Frontend]</Position>
                     </NameBox>
                     <LinkBox>
                         <Link href="https://github.com/An-nettt">
@@ -118,14 +118,14 @@ function TeamGallery() {
                         <UserPhoto src="https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
                     </UserPhotoBox>
                     <NameBox>
-                        <Name>Богдан Потопник</Name>
-                        <Position>Developer [Backend/Frontend]</Position>
+                        <Name>Bohdan Potopnyk</Name>
+                        <Position>Developer [Frontend]</Position>
                     </NameBox>
                     <LinkBox>
-                        <Link href="https://github.com/#">
+                        <Link href="https://github.com/Potopnyk">
                             <IconLink><use href={icon + '#icon-gitHub'}></use></IconLink>
                         </Link>
-                        <Link href="https://t.me/#">
+                        <Link href="https://t.me/Darekas97">
                             <IconLink><use href={icon + '#icon-telegram'}></use></IconLink>
                         </Link>
                     </LinkBox>
