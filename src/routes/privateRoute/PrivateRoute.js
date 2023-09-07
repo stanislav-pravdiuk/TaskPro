db56e5cf9deba62fcacc9caafdc0d5dd3aa77f3d
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-09-07 11:05:27 +0300

    fix
    refactoring routes

--- src/routes/privateRoute/PrivateRoute.js
+++ src/routes/privateRoute/PrivateRoute.js
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import {useAuth} from '../hooks/useAuth';
+import {useAuth} from '../../hooks/useAuth';
 
 export const PrivateRoute = ({ component: Component, redirectTo }) => {
     const { isLoggedIn } = useAuth()
--- src/routes/restrictedRoute/RestrictedRoute.js
+++ src/routes/restrictedRoute/RestrictedRoute.js
@@ -1,4 +1,4 @@
-import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../../hooks/useAuth';
 import { Navigate } from 'react-router-dom';
 
 export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
