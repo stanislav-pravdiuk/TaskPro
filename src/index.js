0492ceea32bbb0530d1b3b905a09c7c1cc011e40
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-09-07 13:37:56 +0300

    rename
    backgrounds

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import './index.css';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      {/* <React.StrictMode> */}
       <BrowserRouter basename="/TaskPro"> 
           <App />       
       </BrowserRouter>
-      {/* </React.StrictMode> */}
     </PersistGate>
   </Provider>
 );
